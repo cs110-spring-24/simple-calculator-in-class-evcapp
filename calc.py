0bc1e68e1d4c4b1c10259f95ff92e9d2865f17d3
evcapp <157657958+evcapp@users.noreply.github.com>
2024-02-06 13:42:22 -0800

    in class (2/5)

--- calc.py
+++ calc.py
@@ -1,1 +1,37 @@
+num1 = int(input("Enter number 1: "))
+op = input("Enter the operator: ")
+num2 = int(input("Enter number 2: "))
 
+if op == "+":
+    total = num1 + num2
+    print(f"{num1} + {num2} = {total}")
+
+elif op == "-":
+    total = num1 - num2
+    print(f"{num1} - {num2} = {total}")
+
+elif op == "*":
+    total = num1 * num2
+    print(f"{num1} * {num2} = {total}")
+
+elif op == "/":
+    if num2 != 0:
+        total = num1 / num2
+        print(f"{num1} / {num2} = {total}")
+    else:
+        print("You can't divide by 0")
+
+elif op == "**":
+    total = num1 ** num2
+    print(f"{num1} ** {num2} = {total}")
+
+elif op == "//":
+    total = num1 // num2
+    print(f"{num1} // {num2} = {total}")
+
+elif op == "%":
+    total = num1 % num2
+    print(f"{num1} % {num2} = {total}")
+
+else:
+    print("Invalid operator") 
